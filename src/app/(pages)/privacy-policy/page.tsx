151feed656376361e8d48958962be1dba3c17073
johnlomat <edwardlomat1503@gmail.com>
2024-11-07 22:07:56 +0800

    Changed: TailwindCSS arbitary class

--- src/app/(pages)/privacy-policy/page.tsx
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -18,42 +18,40 @@ export default async function PrivacyPage() {
       <section className="relative bg-[radial-gradient(ellipse_at_center,_#FFFFFF_5%,#D7D7D7_70%)] py-24 md:pt-40">
         <div className="container flex flex-wrap space-y-6">
           <div className="flex w-full flex-col items-center space-y-20">
-            <div className="w-full max-w-3xl rounded-lg bg-white p-6 shadow-md">
-              <h1 className="mb-4 text-2xl font-bold">Privacy Policy</h1>
-              <p className="mb-4">
+            <div className="w-full max-w-3xl rounded-lg bg-white p-6 shadow-md [&_h1]:mb-4 [&_h1]:text-2xl [&_h1]:font-bold [&_h2]:mb-2 [&_h2]:text-xl [&_h2]:font-semibold [&_p:last-child]:mb-0 [&_p]:mb-4 [&_ul]:mb-4 [&_ul]:list-disc [&_ul]:pl-5">
+              <h1>Privacy Policy</h1>
+              <p>
                 This Privacy Policy explains how we collect, use, disclose, and safeguard your
                 information when you visit our website and use our contact form.
               </p>
-              <p className="mb-4">
+              <p>
                 By accessing or using this website, you agree to the terms of this Privacy Policy.
                 If you do not agree to the terms of this Privacy Policy, please do not access the
                 site.
               </p>
-              <h2 className="mb-2 text-xl font-semibold">1. Information Collection</h2>
-              <p className="mb-4">
+              <h2>1. Information Collection</h2>
+              <p>
                 We collect information you provide directly to us through the contact form on our
                 website. The information we may collect includes:
               </p>
-              <ul className="mb-4 list-disc pl-5">
+              <ul>
                 <li>
                   Personal Data: Personally identifiable information, such as your name, email
                   address, and any other information you choose to provide in your message.
                 </li>
               </ul>
-              <h2 className="mb-2 text-xl font-semibold">2. Use of Information</h2>
-              <p className="mb-4">
-                We may use the information we collect from you in the following ways:
-              </p>
-              <ul className="mb-4 list-disc pl-5">
+              <h2>2. Use of Information</h2>
+              <p>We may use the information we collect from you in the following ways:</p>
+              <ul>
                 <li>To respond to your comments, questions, and provide customer service.</li>
                 <li>To improve our website and services.</li>
               </ul>
-              <h2 className="mb-2 text-xl font-semibold">3. Disclosure of Information</h2>
-              <p className="mb-4">
+              <h2>3. Disclosure of Information</h2>
+              <p>
                 We may share information we have collected about you in certain situations. Your
                 information may be disclosed as follows:
               </p>
-              <ul className="mb-4 list-disc pl-5">
+              <ul>
                 <li>
                   By Law or to Protect Rights: If we believe the release of information about you is
                   necessary to respond to legal process or to protect the rights, property, and
@@ -65,21 +63,21 @@ export default async function PrivacyPage() {
                   to another company.
                 </li>
               </ul>
-              <h2 className="mb-2 text-xl font-semibold">4. Security of Your Information</h2>
-              <p className="mb-4">
+              <h2>4. Security of Your Information</h2>
+              <p>
                 We use administrative, technical, and physical security measures to help protect
                 your personal information. While we have taken reasonable steps to secure the
                 personal information you provide to us, please be aware that despite our efforts, no
                 security measures are perfect or impenetrable, and no method of data transmission
                 can be guaranteed against any interception or other type of misuse.
               </p>
-              <h2 className="mb-2 text-xl font-semibold">5. Changes to This Privacy Policy</h2>
-              <p className="mb-4">
+              <h2>5. Changes to This Privacy Policy</h2>
+              <p>
                 We may update this Privacy Policy from time to time in order to reflect, for
                 example, changes to our practices or for other operational, legal, or regulatory
                 reasons.
               </p>
-              <p className="mb-4">
+              <p>
                 We encourage you to review this Privacy Policy periodically to be informed of how we
                 are protecting your information.
               </p>
--- src/app/(pages)/terms-of-use/page.tsx
+++ src/app/(pages)/terms-of-use/page.tsx
@@ -18,32 +18,32 @@ export default async function TermsPage() {
       <section className="relative bg-[radial-gradient(ellipse_at_center,_#FFFFFF_5%,#D7D7D7_70%)] py-24 md:pt-40">
         <div className="container flex flex-wrap space-y-6">
           <div className="flex w-full flex-col items-center space-y-20">
-            <div className="w-full max-w-3xl rounded-lg bg-white p-6 shadow-md">
-              <h1 className="mb-4 text-2xl font-bold">Terms of Use</h1>
-              <p className="mb-4">
+            <div className="w-full max-w-3xl rounded-lg bg-white p-6 shadow-md [&_h1]:mb-4 [&_h1]:text-2xl [&_h1]:font-bold [&_h2]:mb-2 [&_h2]:text-xl [&_h2]:font-semibold [&_p:last-child]:mb-0 [&_p]:mb-4 [&_ul]:mb-4 [&_ul]:list-disc [&_ul]:pl-5">
+              <h1>Terms of Use</h1>
+              <p>
                 Welcome to my portfolio website. By accessing or using this website, you agree to
                 comply with and be bound by the following terms and conditions of use, which
                 together with our privacy policy govern my relationship with you in relation to this
                 website.
               </p>
-              <p className="mb-4">
+              <p>
                 The term &lsquo;I&lsquo; or &lsquo;me&lsquo; refers to the owner of the website. The
                 term &lsquo;you&lsquo; refers to the user or viewer of this website.
               </p>
-              <h2 className="mb-2 text-xl font-semibold">1. Use of the Website</h2>
-              <p className="mb-4">
+              <h2>1. Use of the Website</h2>
+              <p>
                 The content of the pages of this website is for your general information and use
                 only. It is subject to change without notice.
               </p>
-              <h2 className="mb-2 text-xl font-semibold">2. Intellectual Property</h2>
-              <p className="mb-4">
+              <h2>2. Intellectual Property</h2>
+              <p>
                 This website contains material which is owned by or licensed to me. This material
                 includes, but is not limited to, the design, layout, look, appearance, and graphics.
                 Reproduction is prohibited other than in accordance with the copyright notice, which
                 forms part of these terms and conditions.
               </p>
-              <h2 className="mb-2 text-xl font-semibold">3. User Conduct</h2>
-              <p className="mb-4">
+              <h2>3. User Conduct</h2>
+              <p>
                 You must not misuse this website. You will not: commit or encourage a criminal
                 offense; transmit or distribute a virus, trojan, worm, logic bomb or any other
                 material which is malicious, technologically harmful, in breach of confidence or in
@@ -53,14 +53,14 @@ export default async function TermsPage() {
                 commonly referred to as &quot;spam&quot;; or attempt to affect the performance or
                 functionality of any computer facilities of or accessed through this website.
               </p>
-              <h2 className="mb-2 text-xl font-semibold">4. Limitation of Liability</h2>
-              <p className="mb-4">
+              <h2>4. Limitation of Liability</h2>
+              <p>
                 The information on this website is provided on an &quot;as is&quot; basis. To the
                 fullest extent permitted by law, I exclude all liability for any loss or damage
                 arising out of or in connection with the use of this website.
               </p>
-              <h2 className="mb-2 text-xl font-semibold">5. Changes to Terms</h2>
-              <p className="mb-4">
+              <h2>5. Changes to Terms</h2>
+              <p>
                 I may revise these terms of use at any time without notice. By using this website,
                 you agree to be bound by the current version of these terms of use.
               </p>
